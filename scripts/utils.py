a5a4627c715b4d0566ea00c6cc98165cf703d576
Lee Henderson <7650315+leehenderson@users.noreply.github.com>
2021-01-20 15:37:48 -0600

    from openstates.metadata import metadata

--- scripts/utils.py
+++ scripts/utils.py
@@ -7,7 +7,7 @@ import yaml
 import yamlordereddictloader
 from collections import defaultdict
 from yaml.representer import Representer
-from openstates import openstates.metadata
+from openstates.metadata import metadata
 
 # set up defaultdict representation
 yaml.add_representer(defaultdict, Representer.represent_dict)
